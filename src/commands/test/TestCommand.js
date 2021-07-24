8653e971d92ebe309b45d76fa2cee405aaa4797c
Defeeeee <81602924+Defeeeee@users.noreply.github.com>
2021-07-24 17:52:48 -0300

    Init

--- src/commands/test/TestCommand.js
+++ src/commands/test/TestCommand.js
@@ -1,6 +1,4 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-const Discord = require('discord.js');
-
 
 module.exports = class TestCommand extends BaseCommand {
   constructor() {
@@ -8,10 +6,6 @@ module.exports = class TestCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    const VerifyEmbed = new Discord.MessageEmbed()
-      .setTitle(`Bienvenido a ${message.guild.name}`)
-      .setDescription('Para acceder a todos los mensajes escribi `e!verify`')
-      .setFooter('Defe Bot desarrollado por Federico Diaz Nemeth')
-    message.channel.send(VerifyEmbed)
+    message.channel.send('Test command works');
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+
 const { Client } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const config = require('../slappey.json');
 const client = new Client();
-const keepAlive = require('./server.js');
 
 (async () => {
   client.commands = new Map();
@@ -10,7 +10,6 @@ const keepAlive = require('./server.js');
   client.prefix = config.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  keepAlive();
   await client.login(config.token);
 })();
 
